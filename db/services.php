c11746ddf9cf6b0c43f6d6851a4a7dc616a01b99
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2012-01-11 15:32:39 +1300

    register some webservices method: - get_service_version - get_courses - get_course_participants - get_submissions

--- db/services.php
+++ db/services.php
@@ -17,13 +17,40 @@ $functions = array(
                 'classpath'   => 'local/lightworkws/externallib.php',
                 'description' => 'Return the version number of Lightwork WebService module',
                 'type'        => 'read',
+        ),
+        
+		'local_lightworkws_get_courses' => array(
+                'classname'   => 'local_lightworkws_external',
+                'methodname'  => 'get_courses',
+                'classpath'   => 'local/lightworkws/externallib.php',
+                'description' => 'Return list of courses that this user has access to',
+                'type'        => 'read',
+        ),
+        
+		'local_lightworkws_get_course_participants' => array(
+                'classname'   => 'local_lightworkws_external',
+                'methodname'  => 'get_course_participants',
+                'classpath'   => 'local/lightworkws/externallib.php',
+                'description' => 'Return list of participants to specified courses',
+                'type'        => 'read',
+        ),
+        
+		'local_lightworkws_get_submissions' => array(
+                'classname'   => 'local_lightworkws_external',
+                'methodname'  => 'get_submissions',
+                'classpath'   => 'local/lightworkws/externallib.php',
+                'description' => 'Return list of participants to specified courses',
+                'type'        => 'read',
         )
 );
 
 // We define the services to install as pre-build services. A pre-build service is not editable by administrator.
 $services = array(
         'Lightwork WebServices' => array(
-                'functions' => array ('local_lightworkws_get_service_version'),
+                'functions' => array (
+                		'local_lightworkws_get_service_version',
+                		'local_lightworkws_get_courses'
+                	),
                 'restrictedusers' => 0,
                 'enabled' => 1,
         )
--- externallib.php
+++ externallib.php
@@ -10,22 +10,204 @@
 require_once($CFG->libdir . "/externallib.php");
 
 class local_lightworkws_external extends external_api {
-    
+
     /**
      * Parameters for getServiceVersion method.
-     * @return external_function_parameters 
+     * @return external_function_parameters
      */
     public static function get_service_version_parameters() {
+        return new external_function_parameters(array());
+    }
+
+    public static function get_service_version_returns() {
+        return new external_value(PARAM_TEXT, 'lightwork webservice version');
+    }
+
+    public static function get_service_version() {
+        return "3.x" ;
+    }
+
+    /**
+     * Get Courses
+     */
+    public static function get_courses_parameters() {
         return new external_function_parameters(
-            array()
+            array(
+        		'timemodified' => new external_value(PARAM_INT, 'The last time modified of Courses information this method should return', VALUE_DEFAULT, 0)
+            )
+        );
+    }
+
+    public static function get_courses($timemodified = 0) {
+        $params = self::validate_parameters(self::get_courses_parameters(),
+                        array('timemodified'=>$timemodified));
+        error_log('get_courses() - params: ' . var_export($params, TRUE));
+        
+        $courses = array();
+        
+        for ($i = 0; $i < 2; $i++) {
+            $course_info = array();
+            $course_info['id'] = $i;
+            $course_info['fullname'] = 'Full Course ' . $i;
+            $course_info['shortname'] = 'Short Course ' . $i;
+            $course_info['timemodified'] = $params['timemodified'] + $i;
+            $course_info['assignments'] = array(
+                array(
+                    'id' 		=> $i,
+                    'course'	=> $i,
+                    'name'		=> 'Assignment ' . $i,
+                    'timedue'	=> $i + 100,
+                    'assignmenttype' => 'Random Type',
+                    'grade'		=> 100,
+                    'timemodified' => $params['timemodified'] - $i
+                )
+            );
+                
+            $courses[] = $course_info;
+        }
+        
+        $result = array();
+        $result['courses'] = $courses;
+        $result['errors'] = array();
+        return $result;
+    }
+    
+    private static function assignment_record() {
+        return new external_single_structure(
+            array(
+                'id'		=> new external_value(PARAM_INT, 'assignment id'),
+                'course'	=> new external_value(PARAM_INT, 'course id'),
+                'name'		=> new external_value(PARAM_TEXT, 'assignment name'),
+                'timedue'	=> new external_value(PARAM_INT, 'assignment due time'),
+                'assignmenttype'	=> new external_value(PARAM_TEXT, 'assignment type'),
+                'grade'		=> new external_value(PARAM_INT, 'grade type'),
+				 'timemodified'	    => new external_value(PARAM_INT, 'last time assignment was modified')
+            ), 'assignment information object');
+    }
+    
+    private static function course_record() {
+        return new external_single_structure(
+            array(
+          		'id'		=> new external_value(PARAM_INT, 'course id'),
+           		'fullname'	=> new external_value(PARAM_TEXT, 'course full name'),
+				'shortname' => new external_value(PARAM_TEXT, 'course short name'),
+            	'timemodified' => new external_value(PARAM_INT, 'last time modified'),
+            	'assignments'	=> new external_multiple_structure(self::assignment_record(), 'list of assignment information')
+              ), 'course information object' );
+    }
+        
+    public static function get_courses_returns() {
+        return new external_single_structure(
+            array(
+                'courses'	=> new external_multiple_structure(self::course_record(), 'list of courses information'),
+                'errors'	=> self::errors_structure()
+            )
         );
     }
     
-    public static function get_service_version_returns() {
-        return new external_value(PARAM_TEXT, 'version of Lightwork WebService module');
+    private static function errors_structure() {
+        return new external_multiple_structure( 
+            new external_single_structure( array(
+                'element'	=> new external_value(PARAM_TEXT, 'error element'),
+                'id'		=> new external_value(PARAM_INT, 'error id'),
+                'errorcode' => new external_value(PARAM_TEXT, 'error code'),
+                'errormessage' => new external_value(PARAM_TEXT, 'error message')
+            ), 'errorRecord'), 'list of errorRecords'
+        );
     }
     
-    public static function get_service_version() {
-        return "3.x" ;
+    /**
+     * Get Course Participants
+     */
+    public static function get_course_participants_parameters() {
+        return new external_function_parameters(
+            array(
+                'courseids'		=> new external_multiple_structure(new external_value(PARAM_INT, 'course id')),
+                'timemodified'	=> new external_value(PARAM_INT, 'time modified'),
+                'allstudents'	=> new external_value(PARAM_INT, 'flag to include all students or not')
+            )
+        );
+    }
+    
+    public static function get_course_participants() {
+        
+    }
+    
+    public static function get_course_participants_returns() {
+        return new external_single_structure(
+            array(
+                'courseParticipants' => new external_multiple_structure(self::course_participant(), 'list of courses participant information'),
+                'errors'	         => self::errors_structure()
+            )
+        );
+    }
+    
+    private static function course_participant() {
+        return new external_single_structure(
+            array(
+                'id' => new external_value(PARAM_INT, 'course id'),
+                'user' => new external_multiple_structure(new external_single_structure(
+                    array(
+                        'id'         => new external_value(PARAM_INT, 'user id'),
+                        'username'	 => new external_value(PARAM_TEXT, 'username'),
+                        'idnumber'   => new external_value(PARAM_TEXT, 'idnumber'),
+                        'firstname'  => new external_value(PARAM_TEXT, 'first name'),
+                        'lastname'	 => new external_value(PARAM_TEXT, 'last name'),
+                        'roleid'	 => new external_value(PARAM_INT, 'role id'),
+                        'capabilitycode' => new external_value(PARAM_TEXT, 'capability code'),
+                        'timemodified' => new external_value(PARAM_INT, 'time modified')
+                    )
+                ))
+            )
+        );
+    }
+    
+    /**
+     * Get Submissions
+     */
+    public static function get_submissions_parameters() {
+        return new external_function_parameters(
+            array(
+                'assignmentids' => new external_multiple_structure(new external_value(PARAM_INT, 'assignment id')),
+                'timemodified'	=> new external_value(PARAM_INT, 'time modified'),
+                'allstudents'	=> new external_value(PARAM_INT, 'flag to include all students or not')
+            )
+        );
+    }
+    
+    public static function get_submissions() {
+        
+    }
+    
+    
+    private static function assignment_submissions() {
+        return new external_single_structure(
+            array (
+                'assignmentid'	=> new external_value(PARAM_INT, 'assignment id'),
+                'submissions'   => new external_multiple_structure(new external_single_structure(
+                    array(
+                        'id'		    => new external_value(PARAM_INT, 'submission id'),
+                        'userid'	    => new external_value(PARAM_INT, 'student id'),
+                        'timecreated'   => new external_value(PARAM_INT, 'submission creation time'),
+                        'timemodified'  => new external_value(PARAM_INT, 'submission last modified time'),
+                        'numfiles'	    => new external_value(PARAM_INT, 'number of files in the submission'),
+                        'status'	    => new external_value(PARAM_TEXT, 'submission status'),
+                        'grade'		    => new external_value(PARAM_FLOAT, 'grade'),
+                        'submissioncomment' => new external_value(PARAM_TEXT, 'submission comment'),
+                        'teacher'	    => new external_value(PARAM_INT, 'teacher id'),
+                        'timemarked'    => new external_value(PARAM_INT, 'time the submission was marked')
+                    )
+                ))
+            )
+        );
+    }
+    
+    public static function get_submissions_returns() {
+        return new external_single_structure(
+            array(
+                'assignments' => new external_multiple_structure(self::assignment_submissions(), 'list of assignment submissions'),
+                'errors'	  => self::errors_structure()
+            )
+        );
     }
 }
